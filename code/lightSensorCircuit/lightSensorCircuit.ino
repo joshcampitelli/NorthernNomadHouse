cb308ab74d9b95d74aa12cb9d6880abb38e0f944
Ryan Ribeiro <rmr@ryanribeiro.com>
2019-04-11 13:33:12 -0400

    Add files via upload

---
+++ code/lightSensorCircuit/lightSensorCircuit.ino
@@ -0,0 +1,22 @@
+const int ledPin = 5;
+const int ldrPin = A0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+}
+
+void loop() {
+  int ldrStatus = analogRead(ldrPin);
+  
+  if (ldrStatus <=80) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println(ldrStatus);
+    Serial.println("LDR is DARK, LED is ON");
+  } else {
+    digitalWrite(ledPin, LOW);
+    Serial.println(ldrStatus);
+    Serial.println("LED is OFF");
+  }
+}
